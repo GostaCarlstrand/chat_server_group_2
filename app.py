52b9133cdb06b8351cd95252e34474c11695ff74
Edvin Zackrisson <edvin@knack-knack.se>
2022-01-17 16:38:50 +0100

    Added authenticate, login, create user and functionality in app.py for those html files.

--- app.py
+++ app.py
@@ -3,3 +3,22 @@ from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__)
 
 
+@app.get('/authenticate')
+def authenticate():
+    return render_template("authenticate.html")
+
+
+@app.get('/authenticate/login')
+def user_login():
+    return render_template("login.html")
+
+
+@app.get('/authenticate/create_user')
+def user_create_user():
+    return render_template("create_user.html")
+
+
+@app.post('/authenticate')
+def user_post():
+    return redirect(url_for('user_get'))
+
