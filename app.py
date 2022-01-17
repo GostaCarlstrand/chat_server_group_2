3498fb72f325c0c44f3fbcec18a371242a7cfd00
Gösta <gosta@MacBook-Pro-som-tillhor-Gosta.local>
2022-01-17 16:13:58 +0100

    delete this

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
-def main():
-    pass
+from flask import Flask, Response, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+@app.get("/")
+def home():
+    return render_template("index.html")
 
 
 if __name__ == "__main__":
-    main()
+    app.run()
