7df44b53d2de48cf3156372e0eec98c067abe3e3
Edvin Zackrisson <edvin@knack-knack.se>
2022-01-17 16:56:34 +0100

    Added BASE_URL and created buttons for login and create user.

--- app.py
+++ app.py
@@ -1,24 +1,25 @@
 from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
+BASE_URL = "/"
 
 
-@app.get('/authenticate')
+@app.get(BASE_URL + '/authenticate')
 def authenticate():
     return render_template("authenticate.html")
 
 
-@app.get('/authenticate/login')
+@app.get(BASE_URL + '/authenticate/login')
 def user_login():
     return render_template("login.html")
 
 
-@app.get('/authenticate/create_user')
+@app.get(BASE_URL + '/authenticate/create_user')
 def user_create_user():
     return render_template("create_user.html")
 
 
-@app.post('/authenticate')
+@app.post(BASE_URL + '/authenticate')
 def user_post():
     return redirect(url_for('user_get'))
 
