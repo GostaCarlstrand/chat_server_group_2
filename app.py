a1cb9e59c6f7ef3581e4462cc4be0f3de7f12dcf
Edvin Zackrisson <edvin@knack-knack.se>
2022-01-19 09:19:43 +0100

    Implemented use of db SQLite and functions to check user against the db.

--- app.py
+++ app.py
@@ -1,39 +1,24 @@
 from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
-users = [{'username': 'Edvin', 'password': '1234'}]
-
-
-@app.get('/authenticate')
-def authenticate():
-    return render_template("authenticate.html")
+db = SQLAlchemy()
 
 
-@app.post('/authenticate')
-def authenticate_post_login():
-    username = request.form['user_name']
-    password = request.form['password']
+# We will use a factory function to avoid cyclic imports
+def create_app():
+    app = Flask(__name__)
+    # Many parts of flask will require us to use a secret key so we created one
+    app.config['SECRET_KEY'] = 'myS3cr37k3y'
+    # We will configure SQLAlchemy to use SQLite and the file db.sqlite
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 
-    for user in users:
-        if user['username'] == username and user['password'] == password:
-            return redirect(url_for('user_login'))
-    return render_template("unauthorized.html")
+    # Init the SQLAlchemy object with our app object
+    db.init_app(app)
 
+    from blueprints.open import bp_open
+    app.register_blueprint(bp_open)
 
-@app.get('/create_user')
-def user_post():
-    return render_template("create_user.html")
+    from blueprints.user import bp_user
+    app.register_blueprint(bp_user)
 
-
-@app.get('/authenticate/login')
-def user_login():
-    return render_template("login.html")
-
-
-@app.get('/authenticate/create_user')
-def user_create_user():
-    return render_template("create_user.html")
-
-
-if __name__ == "__main__":
-    app.run()
+    return app
