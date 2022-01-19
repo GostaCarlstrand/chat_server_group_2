fda948a411656f480063a9a22c8170d8964f47eb
Gösta <gosta@MacBook-Pro-som-tillhor-Gosta.local>
2022-01-19 11:31:29 +0100

    working on signin with javascript

--- static/js/source.js
+++ static/js/source.js
@@ -1,3 +1,29 @@
-function changeText() {
-    document.getElementById("main-heading").innerHTML = "New text";
+function verifyPassword() {
+  var pw_repeated = document.getElementById("pswd_repeat").value;
+  var pw = document.getElementById("pswd").value;
+  //check empty password field
+  if(pw == "") {
+     document.getElementById("pswd_notification").innerHTML = "Fill the password please!";
+     return false;
+  }
+
+ //minimum password length validation
+  if(pw.length < 8) {
+     document.getElementById("pswd_notification").innerHTML = "Password length must be at least 8 characters";
+     return false;
+  }
+  if(pw.length > 15) {
+      document.getElementById("pswd_notification").innerHTML = "Password length must not exceed 15 characters";
+      return false;
+  }
+
+
+  if (pw == pw_repeated) {
+      window.location.replace()
+  } else {
+      document.getElementById("pswd_notification").innerHTML = "Password does not match";
+      return false;
+  }
+
+
 }
