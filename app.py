b2cccf5a2509efc19667c53ffe933bdbb1724c05
Edvin Zackrisson <edvin@knack-knack.se>
2022-02-04 08:05:30 +0100

    C

--- app.py
+++ app.py
@@ -1,39 +1,47 @@
+import dotenv
 from flask import Flask, render_template, request, redirect, url_for
+from flask_login import LoginManager
+from flask_sqlalchemy import SQLAlchemy
+from flask_mqtt import Mqtt
 
-app = Flask(__name__)
-users = [{'username': 'Edvin', 'password': '1234'}]
-
-
-@app.get('/authenticate')
-def authenticate():
-    return render_template("authenticate.html")
+db = SQLAlchemy()
 
 
-@app.post('/authenticate')
-def authenticate_post_login():
-    username = request.form['user_name']
-    password = request.form['password']
-
-    for user in users:
-        if user['username'] == username and user['password'] == password:
-            return redirect(url_for('user_login'))
-    return render_template("unauthorized.html")
+mqtt = Mqtt()
 
 
-@app.get('/create_user')
-def user_post():
-    return render_template("create_user.html")
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'qwerty'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+
+    login_manager = LoginManager()
+
+    # Init the login manager with our app object
+    login_manager.init_app(app)
+
+    # Create a user_loader function. Used by flask-login
+    @login_manager.user_loader
+    def load_user(user_id):
+        from models import User
+        return User.query.filter_by(id=user_id).first()
+
+    from blueprints.open import bp_open
+    app.register_blueprint(bp_open)
+
+    from blueprints.user import bp_user
+    app.register_blueprint(bp_user)
+
+    from blueprints.api import bp_api
+    app.register_blueprint(bp_api, url_prefix='/api/v1.0')
+
+    return app
 
 
-@app.get('/authenticate/login')
-def user_login():
-    return render_template("login.html")
+if __name__ == '__main__':
+    dotenv.load_dotenv()
+    app = create_app()
+    app.run(port=5010)
 
-
-@app.get('/authenticate/create_user')
-def user_create_user():
-    return render_template("create_user.html")
-
-
-if __name__ == "__main__":
-    app.run()
--- static/js/source.js
+++ static/js/source.js
@@ -1,4 +1,36 @@
-function changeText() {
-    document.getElementById("main-heading").innerHTML = "New text";
+<<<<<<< HEAD
+=======
+function verifyPassword() {
+  var pw_repeated = document.getElementById("pswd_repeat").value;
+  var pw = document.getElementById("pswd").value;
+  //check empty password field
+  if(pw == "") {
+     document.getElementById("pswd_notification").innerHTML = "Fill the password please!";
+     return false;
+  }
+
+ //minimum password length validation
+  if(pw.length < 8) {
+     document.getElementById("pswd_notification").innerHTML = "Password length must be at least 8 characters";
+     return false;
+  }
+  if(pw.length > 15) {
+      document.getElementById("pswd_notification").innerHTML = "Password length must not exceed 15 characters";
+      return false;
+  }
+
+
+  if (pw == pw_repeated) {
+      window.location.replace()
+  } else {
+      document.getElementById("pswd_notification").innerHTML = "Password does not match";
+      return false;
+  }
+
 
 }
+
+function reload_page() {
+    window.location.reload();
+}
+>>>>>>> 4ef7b52e7c724dadf8c5e86ebdee4aff50d13b26
